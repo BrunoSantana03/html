875d3a27d3e0e29e87caba871ade6317f4a2dfde
BrunoSantana03 <166453824+BrunoSantana03@users.noreply.github.com>
2024-05-13 14:41:05 -0300

    Add files via upload

---
+++ exercicios.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Exercícios</title>
+  </head>
+  <body>
+	<h1>Exercícios</h1>
+	<ol>
+	<li>Escreva um código em PHP para mostrar o seu nome na tela.<p>
+<?php
+  echo "Unileste";
+?>
+<p><b>código</b></p>
+<p><pre> echo "unileste" </pre></p>
+		<li>Escreva um código em PHP para mostrar os números de 1 até 100 na tela. 
+	</ol>
+<?php
+?>
+ 
+
+
+
+</body>
+</html>
