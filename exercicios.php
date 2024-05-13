644b9a1c85e0fa640f75db1bbb9040c8e5c82953
BrunoSantana03 <166453824+BrunoSantana03@users.noreply.github.com>
2024-05-13 15:01:24 -0300

    Update exercicios.php

--- exercicios.php
+++ exercicios.php
@@ -1,18 +1,12 @@
 <!doctype html>
 <html>
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
     <title>Exercícios</title>
   </head>
-  <body>
-	<h1>Exercícios</h1>
-	<ol>
-	<li>Escreva um código em PHP para mostrar o seu nome na tela.</li>
-<?php
-$nome = "Seu Nome";
-echo "Meu nome é $nome";
-?>
-	</ol>
+  
+<body>
+
+<?php echo "Meu nome é "; ?>
+
 </body>
 </html>
